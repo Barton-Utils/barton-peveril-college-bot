eaf4ff58561318cf5599f21c4625dc4aa06550c2
Rubens P <rubens.pirie@gmail.com>
2021-08-23 16:59:47 +0100

    added verification embed

--- src/commands/discord/util/test.js
+++ src/commands/discord/util/test.js
@@ -1,5 +1,5 @@
 const { oneLine } = require('common-tags');
-
+const Discord = require('discord.js')
 class Command {
 	constructor(parent, client) {
 		this.parent = parent;
@@ -57,10 +57,18 @@ class Command {
 
 	async finally(client, chain, message) {
 		return true;
-	}
+	}	
 
 	async execute(client, chain, message, args) {
-		console.log(await message.guild.commands.set([]));
+		message.channel.send({ embeds: [ new Discord.MessageEmbed()
+			.setTitle('Welcome to the Discord!')
+			.setColor('BLUE')
+			.setDescription('Welcome to the unofficial Barton Peveril College Discord server. This is the only piece of discord verification you will have to do however you will need to do it in order to gain access to the rest of the discord server and there’s no way around it! If you need help that’s not explained by the steps below contact a admin. If your confused, please follow the steps below.')
+			.setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+			.addField('How to Verify:', ':one: Make sure you have access to your barton email account.\n\n:two: Copy your email address (E.G. 21RP0909@barton.ac.uk) and paste it into this channel.\n\n:three: If your email was correct you should get a confirmation message if not **READ** the error message.\n\n:four: Check your barton email, in there should be a email with the subject `Discord Account Verification` open it and click the link.\n\n:five: This should take you to a confirmation site, if you see a confirmed message then your all set to go!\n\n\n_If you get an error message then send it to <@436876982794452992> and he\'ll give you a hand!_')
+			.setTimestamp()
+			.setFooter('Barton Peveril College Discord', client.user.displayAvatarURL({ dynamic: true}))
+		] })
 	}
 
 	async slash(client, interaction) {
